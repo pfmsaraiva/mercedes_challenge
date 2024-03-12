85f8c41615365113fcfc21e0ec24713dc806bf03
Pedro Saraiva <pedrosaraiva@Pedros-Air.lan>
2024-03-12 19:27:54 +0000

    Add some text to search area

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,9 +4,18 @@ describe('My First Test', () => {
   })
 })
 
-describe('Open Google Test', function() {
-  it('Visits Google homepage', function() {
+describe('Open Google Test', () => {
+  it('Visits Google homepage', () => {
   //Visit the Demo QA Website
   cy.visit("https://www.google.com/");
+  // Reject Cookies
+  cy.get('button[id="W0wltc"]').click();
+  //Click Searchbox
+  cy.get('textarea[name="q"]').click();
+  //Write something
+  cy.get('textarea[name="q"]').type('Something funny');
+  //Search
+  //cy.get('input[type="Submit"]').click();
+  //Confirm that page has something on the results
 })
 })
